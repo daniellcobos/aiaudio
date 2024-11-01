f1dfdc027667978a52ccb50f8c3001e85efc2929
Daniel Leonardo Cobos Prada <daniel.leocobos@gmail.com>
2024-11-01 12:13:51 -0500

    more stuff

--- main.py
+++ main.py
@@ -20,35 +20,53 @@ import torch
 
 app = Flask(__name__, static_url_path = '/static')
 CORS(app)
-
-
+import openai
 app.config.from_object(config.config['development'])
 
-
+API = app.config['OPENAI_API_KEY']
 oauth.init_app(app)
 sqla.init_app(app)
 login_manager.init_app(app)
 app.register_blueprint(auth.bp)
 
+client = openai.OpenAI(api_key=API)
 
 @app.route('/')
-@login_required
+
 def index():
 
     return render_template('index.html')
 
 
 @app.route('/uploader', methods = ['GET', 'POST'])
-@login_required
+
 def uploader():
     torch.cuda.init()
-    device = "cpu"
+    device = "cuda"
     if request.method == 'POST':
-        mpath = path.join(app.root_path, 'static', 'uploads',str(session['Cliente']))
+        mpath = path.join(app.root_path, 'static', 'uploads',str("temp"))
         if not os.path.exists(mpath):
                os.makedirs(mpath)
         uploaded_files = request.files.getlist("archivo")
         for file in uploaded_files:
+
+            nombre = file.filename.split('.')[0]
+            filename = secure_filename(file.filename)
+            fpath = path.join(mpath, filename)
+            file.save(fpath)
+            audio_file = open(fpath, "rb")
+            transcript = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_file,
+                response_format="text"
+            )
+            transfile = path.join(mpath, 'textos', nombre + ".txt")
+            with open(transfile, 'w+', encoding="utf-8") as f:
+                f.write(transcript)
+            formatear_texto(transfile)
+
+            """
+
             print("Cargando" + file.filename)
             filename = secure_filename(file.filename)
             fpath = path.join(mpath,filename)
@@ -56,9 +74,10 @@ def uploader():
 
             nombre = file.filename.split('.')[0]
             tipo = file.filename.split('.')[-1]
-            print(tipo)
+
             sonido = nombre + ".mp3"
             spath = path.join(mpath, sonido)
+            tipo = tipo.lower()
             if ( tipo != "mp3" ):
 
                 import moviepy.editor
@@ -80,12 +99,13 @@ def uploader():
             result = model.transcribe(spath)
 
             print(result["text"])
-            transfile = path.join(mpath,nombre +".txt")
+            transfile = path.join(mpath,'textos',nombre +".txt")
             with open(transfile, 'w+', encoding="utf-8") as f:
                 f.write(result["text"])
-
+            
             formatear_texto(transfile)
 
+             """
 
         return 'Files uploaded and proceed successfully'
    
